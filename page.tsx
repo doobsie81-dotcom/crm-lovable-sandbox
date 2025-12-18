ac4f512292cb0721eb183044fac1c3ca71bc5850
doobsie81-dotcom <doobsie81@gmail.com>
2025-12-18 12:23:03 +0200

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -2,14 +2,14 @@ import { Suspense } from "react";
 import { HydrateClient } from "~/trpc/server";
 import { ErrorBoundary } from "react-error-boundary";
 import FallbackRender from "~/components/error-boundary-fallback";
-import CompaniesOverview from "./_components/companies-overview";
+import InvoicesOverview from "./_components/invoice-overview";
 
-export default async function CompaniesOverviewPage() {
+export default async function InvoicesPage() {
   return (
     <HydrateClient>
       <ErrorBoundary FallbackComponent={FallbackRender}>
         <Suspense fallback={<div>Loading...</div>}>
-          <CompaniesOverview />
+          <InvoicesOverview />
         </Suspense>
       </ErrorBoundary>
     </HydrateClient>
