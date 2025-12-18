ff731c22d3a4e15a8b90d13f142cf42b56b2b752
doobsie81-dotcom <doobsie81@gmail.com>
2025-12-18 12:30:02 +0200

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,17 +1,33 @@
-import { Suspense } from "react";
-import { HydrateClient } from "~/trpc/server";
-import { ErrorBoundary } from "react-error-boundary";
-import FallbackRender from "~/components/error-boundary-fallback";
-import InvoicesOverview from "./_components/invoice-overview";
+import { getServerSession } from "~/lib/auth";
+import { redirect } from "next/navigation";
+import PageHeader from "~/components/page-header";
+import { Dashboard } from "./_dashboard";
+import { getUserPermissions } from "~/lib/get-user-permissions";
 
-export default async function InvoicesPage() {
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session?.user) {
+    redirect("/unauthorized");
+  }
+
+  const abilities = await getUserPermissions(
+    session?.user,
+    session.userPermissions
+  );
+
+  // we need to have a set of redirects here...
+  if (!abilities.can("read", "Dashboard")) {
+    redirect("/pipeline");
+  }
+
   return (
-    <HydrateClient>
-      <ErrorBoundary FallbackComponent={FallbackRender}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <InvoicesOverview />
-        </Suspense>
-      </ErrorBoundary>
-    </HydrateClient>
+    <main className="space-y-6">
+      <PageHeader
+        title="Admin Dashboard"
+        subtitle="Overview of system metrics"
+      />
+      <Dashboard />
+    </main>
   );
 }
